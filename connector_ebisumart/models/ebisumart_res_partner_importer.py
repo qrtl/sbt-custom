fe497f7b6dd5d6c67667f6fcf88622d10a375b69
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-08-25 19:27:06 +0800

    upd

--- connector_ebisumart/models/ebisumart_res_partner_importer.py
+++ connector_ebisumart/models/ebisumart_res_partner_importer.py
@@ -41,44 +41,5 @@ class EbisumartResPartnerImporter(Component):
     _inherit = 'ebisumart.importer'
     _apply_on = 'ebisumart.res.partner'
 
-    # def _get_partner_binding(self, partner_type):
-    #     """ Get the binding for a particular partner type """
-    #     return self.env['ebisumart.res.partner'].search([
-    #         ('external_id', '=', self.external_id),
-    #         ('partner_type', '=', partner_type)
-    #     ], limit=1)
-
-    # def _run_partner_import(self, force=False):
-    #     """ Special handling for importing partners """
-
-    #     map_record = self._map_data()
-
-    #     for partner_type in ['customer', 'supplier']:
-    #         # Search for existing binding
-    #         binding = self._get_partner_binding(partner_type)
-            
-    #         if not force and self._is_uptodate(binding):
-    #             return _('Already up-to-date.')
-    #         # If binding exists, update associated partner
-    #         if binding:
-    #             partner_vals = self._update_data(map_record)
-    #             partner_vals.update({partner_type: True})
-    #             binding.odoo_id.write(partner_vals)
-    #         else:
-    #             # If not, create a new binding and associated partner
-    #             partner_vals = self._create_data(map_record)
-    #             partner_vals.update({'customer': False})
-    #             partner_vals.update({'supplier': False})
-    #             partner_vals.update({partner_type: True})
-    #             partner = self.env['res.partner'].create(partner_vals)
-    #             binding_vals = {
-    #                 'odoo_id': partner.id,
-    #                 'external_id': self.external_id,
-    #                 'partner_type': partner_type,
-    #                 'backend_id': self.backend_record.id
-    #             }
-    #             self.env['ebisumart.res.partner'].create(binding_vals)
-    #     self._after_import(binding)
-
     def _after_import(self, binding):
         binding.odoo_id._after_import()
