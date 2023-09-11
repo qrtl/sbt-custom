a82c0666a077e52058bbfd828b3af87ad07e5dd2
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-09-11 22:51:56 +0800

    update

--- connector_ebisumart/models/ebisumart_sale_order.py
+++ connector_ebisumart/models/ebisumart_sale_order.py
@@ -37,6 +37,29 @@ class SaleOrder(models.Model):
     )
     cancel_in_ebisumart = fields.Boolean()
 
+    def _after_import(self):
+        product_ids = self.order_line.mapped('product_id')
+        for product in product_ids:
+            vendor = self.env["res.partner"].search(
+            [
+                ('ebisumart_id', '=', product.torihikisaki_id),
+                ('supplier', '=', True)
+            ], limit=1)
+
+            existing_supplierinfo = self.env['product.supplierinfo'].search([
+                ('name', '=', vendor.id),
+                ('product_tmpl_id', '=', product.product_tmpl_id.id),
+                ('price', '=', product.list_price)
+            ])
+            # If no existing supplierinfo with the same price, supplier and product
+            if not existing_supplierinfo:
+                self.env['product.supplierinfo'].create({
+                    'name': vendor.id,
+                    'price': product.list_price,
+                    'product_tmpl_id': product.product_tmpl_id.id,
+                })
+
+
 class EbisumartSaleOrderLine(models.Model):
     _name = 'ebisumart.sale.order.line'
     _inherit = 'ebisumart.binding'
