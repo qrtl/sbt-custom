a582dc25bcc958a5d4dcc609614cad4cd229547c
Aungkokolin1997 <aungkokolin23397@gmail.com>
2024-01-31 18:30:50 +0800

    upd

--- sbt_sale/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of Pactera. See LICENSE file for full copyright and licensing details.
-
-from . import models
--- sbt_sale/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Pactera. See LICENSE file for full copyright and licensing details.
-
-from . import AccountJounal
-from . import account_invoice
-from . import res_currency
-from . import res_partner
